7518c53de1774c31d8d3dd8639ef4d64b0ab312f
yiduang <517109829@qq.com>
2020-03-17 13:34:57 +0800

    20200317 1334 upload
    20200317 1334 upload

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -16,7 +16,7 @@
       <el-button @click="router_to('/makecomments')">发表评论</el-button>
       <!-- <el-button @click="router_to('/userinfo')">用户资料</el-button> -->
       <!-- <el-button @click="router_to('/shippingaddress')">收货地址</el-button> -->
-      <magnifier></magnifier>
+      <magnifier :imgList="pictureList2"></magnifier>
     </div>
   </div>
 </template>
@@ -31,7 +31,34 @@ export default {
     Magnifier
   },
   data() {
-    return {};
+    return {
+      pictureList2: [
+        {
+          ImgUrl:
+            "http://mp.ofweek.com/Upload/News/Img/member645/201711/17170046839337.jpg"
+        },
+        {
+          ImgUrl:
+            "http://image.buy.ccb.com/merchant/201703/904919627/1522929521661_4.jpg"
+        },
+        {
+          ImgUrl:
+            "http://image5.suning.cn/uimg/b2c/newcatentries/0070130691-000000000826244625_5_800x800.jpg"
+        },
+        {
+          ImgUrl:
+            "http://img12.360buyimg.com/n5/s450x450_jfs/t9952/98/2269407420/279171/6137fe2f/59f28b2bN6959e086.jpg"
+        },
+        {
+          ImgUrl:
+            "http://d.ifengimg.com/w600/p0.ifengimg.com/pmop/2017/1213/A4037864F6728F006B67AAEC51EC8A485F320FD2_size93_w1024_h734.jpeg"
+        },
+        {
+          ImgUrl:
+            "http://d.ifengimg.com/w600/p0.ifengimg.com/pmop/2017/1213/A4037864F6728F006B67AAEC51EC8A485F320FD2_size93_w1024_h734.jpeg"
+        }
+      ]
+    };
   },
   mounted() {
     let vm = this;
